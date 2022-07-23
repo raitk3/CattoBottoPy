37f0938c4407395a4c246cc2a4845b94e31270cc
Rait Kulbok <raitsprait@gmail.com>
2022-07-23 13:27:05 +0300

    Added new token for now, will make it into a variable soon

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 
-TOKEN = 'OTEzNzkyNjI0MDQ4MDc4ODY4.YaDpsw.KiosXTUVjh_xcRVEXyk6zXQqK5M'
+TOKEN = 'OTEzNzkyNjI0MDQ4MDc4ODY4.GEU918.n846Q6FjzLyLdWSI6N2vjRgyTbqhqBQJpeOgO4'
+PERMISSIONS = '1445987360'
 
 description = '''Rait wanted to do a bot, so uhh...here it is!'''
 bot = commands.Bot(command_prefix='!', description=description)
@@ -43,4 +44,25 @@ async def leave_music(ctx):
     """Leaves vc"""
     await ctx.voice_client.disconnect()
 
+def is_user(message, user, override):
+    if override:
+        return True
+    author = str(message.author.id)
+    user_strip = user[2:-1]
+    print(f"Message author: {author}, user to purge: {user_strip}")
+    return author == user_strip
+
+@bot.command()
+async def purge(ctx, number, user=""):
+    override = user == ""
+    number_of_messages = min(100, int(number))
+    await ctx.send(f"Removing messages from {user} from last {number_of_messages} messages")
+    channel = ctx.channel
+    deleted = await channel.purge(limit=number_of_messages, check=lambda x, y=user, o=override: is_user(x, y, o))
+    await ctx.send('Deleted {} message(s)'.format(len(deleted)))
+
+@bot.command()
+async def junk(ctx, number):
+    for i in range(int(number)):
+        await ctx.send(i)
 bot.run(TOKEN)
